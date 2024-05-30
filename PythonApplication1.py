23f2f95fa275cac899c86f6b91caff2764a90179
qasdew8750 <163403562+qasdew8750@users.noreply.github.com>
2024-05-31 06:43:27 +0900

    Add files via upload

---
+++ PythonApplication1.py
@@ -0,0 +1,131 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.fuctional as F
+import torchvision
+import torchvision.transforms as transforms
+
+Parameters
+seed = 42
+Learning_rate = 0.001
+size = 32
+epochs = 10
+im_size = 32
+N_classes = 10
+file = "cifar10.gz"
+
+class LeNet5(nn.Module):
+    def __init__(self,n_classes):
+         self.feature_extractor = nn.Sequential(            
+            nn.Conv2d(in_channels=1, out_channels=6, kernel_size=5, stride=1),
+            nn.Tanh(),
+            nn.AvgPool2d(kernel_size=2),
+            nn.Conv2d(in_channels=6, out_channels=16, kernel_size=5, stride=1),
+            nn.Tanh(),
+            nn.AvgPool2d(kernel_size=2),
+            nn.Conv2d(in_channels=16, out_channels=120, kernel_size=5, stride=1),
+            nn.Tanh()
+            nn.fc1 = nn.Linear(256, 120)
+            nn.Tanh()
+            nn.fc2 = nn.Linear(120, 84)
+            nn.Tanh()
+            nn.fc3 = nn.Linear(84, 10)
+            nn.Tanh()
+        )
+    def forward(self, x):
+        y = self.conv1(x)
+        y = self.relu1(y)
+        y = self.pool1(y)
+        y = self.conv2(y)
+        y = self.relu2(y)
+        y = self.pool2(y)
+        y = y.view(y.shape[0], -1)
+        y = self.fc1(y)
+        y = self.relu3(y)
+        y = self.fc2(y)
+        y = self.relu4(y)
+        y = self.fc3(y)
+        y = self.relu5(y)
+        return y
+
+train_set = torchvision.datasets.MNIST(
+    root = file,
+    train = True,
+    download = True,
+    transform = transfroms.Compose([
+        transfroms.ToTensor() 
+    ])
+)
+test_set = torchvision.datasets.MNIST(
+    root = file,
+    train = False,
+    download = True,
+    transform = transfroms.Compose([
+        transfroms.ToTensor() 
+    ])
+)
+ 
+# train_loader, test_loader »ý¼º
+train_loader = torch.utils.data.DataLoader(train_set, batch_size=batch_size)
+test_loader = torch.utils.data.DataLoader(test_set, batch_size=batch_size)
+
+def train(epoch):
+    global cur_batch_win
+    net.train()
+    loss_list, batch_list = [], []
+    for i, (images, labels) in enumerate(data_train_loader):
+        optimizer.zero_grad()
+
+        output = net(images)
+
+        loss = criterion(output, labels)
+
+        loss_list.append(loss.detach().cpu().item())
+        batch_list.append(i+1)
+
+        if i % 10 == 0:
+            print('Train - Epoch %d, Batch: %d, Loss: %f' % (epoch, i, loss.detach().cpu().item()))
+
+        # Update Visualization
+        if viz.check_connection():
+            cur_batch_win = viz.line(torch.Tensor(loss_list), torch.Tensor(batch_list),
+                                     win=cur_batch_win, name='current_batch_loss',
+                                     update=(None if cur_batch_win is None else 'replace'),
+                                     opts=cur_batch_win_opts)
+
+        loss.backward()
+        optimizer.step()
+
+
+def test():
+    net.eval()
+    total_correct = 0
+    avg_loss = 0.0
+    for i, (images, labels) in enumerate(data_test_loader):
+        output = net(images)
+        avg_loss += criterion(output, labels).sum()
+        pred = output.detach().max(1)[1]
+        total_correct += pred.eq(labels.view_as(pred)).sum()
+
+    avg_loss /= len(data_test)
+    print('Test Avg. Loss: %f, Accuracy: %f' % (avg_loss.detach().cpu().item(), float(total_correct) / len(data_test)))
+
+
+def train_and_test(epoch):
+    train(epoch)
+    test()
+
+    dummy_input = torch.randn(1, 1, 32, 32, requires_grad=True)
+    torch.onnx.export(net, dummy_input, "lenet.onnx")
+
+    onnx_model = onnx.load("lenet.onnx")
+    onnx.checker.check_model(onnx_model)
+
+
+def main():
+    for e in range(1, 16):
+        train_and_test(e)
+
+
+if __name__ == '__main__':
+    main()
